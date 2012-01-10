dbe8932a2e1635a88bce1e497b12b23c9973e952
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2012-01-10 12:48:46 -0600

    Added new files for new version of trunk vs trunk

---
+++ scripts/dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#export LSST_HOME=/lsst/DC3/stacks/gcc445-RH6/15nov2011
+#export LSST_HOME=/lsst/DC3/stacks/gcc445-RH6/28nov2011
+BUILDBOT_HOME=$HOME/RHEL6
+source $LSST_HOME/loadLSST.sh
+which python
+echo $PYTHON_PATH
+eups list
+python $BUILDBOT_HOME/scripts/dependencies.py
---
+++ scripts/prepareManifestPackages.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+BUILDBOT_HOME=$HOME/RHEL6
+source $LSST_HOME/loadLSST.sh
+ARGS=$*
+echo "ARGS ARE: $ARGS"
+cat manifest.list | while read LINE; do
+    set $LINE
+    $BUILDBOT_HOME/scripts/gitPrepareManifestPackage.sh $ARGS --step_name $1 $1
+done
