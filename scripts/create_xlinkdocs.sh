d3b4e8f4c15926f1a0bbce75aec13baa26684bde
Robyn Allsman <robyn@LSST.org>
2012-01-20 15:07:24 -0600

    Updated to build doxygen documentation for stable,beta, and master branches using new RHL script aggregating the build stack package documentation.

--- scripts/create_xlinkdocs.sh
+++ scripts/create_xlinkdocs.sh
@@ -2,15 +2,21 @@
 # Build cross linked doxygen documents and load into website
 
 usage() {
-    echo "Usage: $0 type destination url_prefix "
+    echo "Usage: $0 type destination"
     echo "Build crosslinked doxygen documentation and install on LSST website."
-    echo "          type: either \"trunk\" or \"current\""
-    echo "   destination: an scp target -- username, host & directory to scp files to,"
-    echo "                such as \"buildbot@willy.ncsa.illinois.edu:/var/www/html/doxygen\""
-    echo "    url_prefix: the beginning of a URL that points to the destination,"
-    echo "                such as \"http://dev.lsstcorp.org/doxygen\""
+    echo "             type: either \"trunk\" \"stable\" \"beta\""
+    echo "   <host>:/<path>: an scp target for user 'buildbot' "
+    echo "                   where host & fullpath directory specify where to receive scp files,"
+    echo "                         the remote account is pre-defined to 'buildbot'."
+    echo "                   example: \"lsst-build.ncsa.illinois.edu:/var/www/html/doxygen\""
 }
 
+#----------------------------------------------------------------------------- 
+#  To manually invoke this builbot script, do:
+# % <setup eups>
+# % cd <buildbot work directory>
+# % ~/RHEL6/gitwork/scripts/create_xlinkdocs.sh beta lsst-build5.ncsa.illinois.edu:/lsst/home/buildbot/public_html/doxygen
+#----------------------------------------------------------------------------- 
 check1() {
     if [ "$1" = "" -o "${1:0:1}" = "-" ]; then
         usage
@@ -19,25 +25,22 @@ check1() {
 }
 
 source $LSST_HOME/loadLSST.sh
-#source /usr/local/home/buildbot/buildslave/Release/work/loadLSST.sh
 
-source ${0%/*}/build_functions.sh
+source ${0%/*}/gitBuildFunctions.sh
 
 DEBUG=debug
 DEV_SERVER="lsstdev.ncsa.uiuc.edu"
-SVN_SERVER="svn.lsstcorp.org"
-#WEB_HOST="willy.ncsa.illinois.edu"
-#WEB_ROOT="/var/www/html/doxygen"
-WEB_HOST="lsst-build.ncsa.illinois.edu"
-WEB_ROOT="/usr/local/home/buildbot/www/"
+SCM_SERVER="git.lsstcorp.org"
+WEB_HOST="lsst-build5.ncsa.illinois.edu"
+WEB_ROOT="/lsst/home/buildbot/public_html/doxygen"
 
 # -------------------
 # -- get arguments --
 # -------------------
 #*************************************************************************
 check1 $@
-DOXY_TYPE=$1           # 'trunk' or 'current'
-if [ "$DOXY_TYPE" != "trunk"  -a "$DOXY_TYPE" != "current" ]; then
+DOXY_TYPE=$1           # 'trunk' or 'stable'
+if [ "$DOXY_TYPE" != "trunk"  -a "$DOXY_TYPE" != "stable" -a "$DOXY_TYPE" != "beta" ]; then
     usage
     exit 1
 fi
@@ -47,78 +50,138 @@ shift
 
 #*************************************************************************
 check1 $@
-DESTINATION=$1         # buildbot@willy.ncsa.illinois.edu:/var/www/html/doxygen
-REMOTE_HOST=${DESTINATION%%\:*} # buildbot@willy.ncsa.illinois.edu
-REMOTE_DIR=${DESTINATION##*\:}  # /var/www/html/doxygen
-shift
-#*************************************************************************
-check1 $@
-URL_PREFIX=$1
+DESTINATION=$1         # buildbot@lsst-build5.ncsa.illinois.edu:/lsst/home/buildbot/public_html/doxygen
+REMOTE_USER="buildbot"
+REMOTE_HOST="${DESTINATION%%\:*}" # lsst-build5.ncsa.illinois.edu
+REMOTE_DIR="${DESTINATION##*\:}"  # /var/www/html/doxygen
 shift
 #*************************************************************************
 
 DATE="`date +%Y`_`date +%m`_`date +%d`_`date +%H.%M.%S`"
 
 echo "DATE: $DATE"
+echo "REMOTE_USER $REMOTE_USER"
 echo "DESTINATION: $DESTINATION"
 echo "REMOTE_HOST: $REMOTE_HOST"
 echo "REMOTE_DIR: $REMOTE_DIR"
 echo "DOXY_TYPE: $DOXY_TYPE"
 
-# svn checkout devenv_doc ** from trunk **
-prepareSvnDir devenv_doc
-if [ $RETVAL != 0 ]; then
-    echo "devenv_doc svn checkout or update failed; contact the LSST buildbot developer"
+
+ssh $REMOTE_USER@$REMOTE_HOST pwd
+if [ $? != 0 ]; then
+    echo "$DESTINATION  does not resolve to a valid URL for account buildbot:  buildbot@<host>:<fullpath>"
+    usage
     exit 1
 fi
 
-# devenv_doc should have its eups table setup all packages in Latest Release
-cd $SVN_LOCAL_DIR
+ssh $REMOTE_USER@$REMOTE_HOST  test -e $REMOTE_DIR 
+if [ $? != 0 ]; then
+    echo "Executed: ssh $REMOTE_USER@$REMOTE_HOST  test -e $REMOTE_DIR"
+    echo "Failed to find: $REMOTE_DIR, is the directory valid?"
+    usage
+    exit 1
+fi
+
+WORK_DIR=`pwd`
+echo "WORK_DIR: $WORK_DIR"
+
+# SCM checkout devenv/lsstDoxygen ** from trunk **
+prepareSCMDirectory devenv/lsstDoxygen BUILD
+if [ $RETVAL != 0 ]; then
+    echo "Failed to extract $PACKAGE source directory during setup for BUILD."
+    exit 1
+fi
+
+# setup all packages required by devenv/lsstDoxygen's eups
+cd $SCM_LOCAL_DIR
+echo "SCM_LOCAL_DIR: $SCM_LOCAL_DIR"
 setup -r .
-eups list  -s
+eups list -s
 
 # Create doxygen output for ALL eups-setup packages
 export xlinkdoxy=1
-pretty_execute scons doxygen
-if [ $RETVAL != 0 ]; then
-    echo "Build of cross-linked doxygen documentation failed."
+
+scons 
+if [ ! $? ]; then
+    echo "Failed to build lsstDoxygen package."
     exit 1
 fi
-cd doxygen
 
-# rename the htmlDir 
+# Now setup for build of Data Release library documentation
+echo ""
+eups list -v datarel
+echo ""
+
+#/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+#   F U T U R E  E R R O R :  trunk and release version should both be handled
+#/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+if [ "$DOXY_TYPE" = "trunk" ] ; then 
+    STACK_TYPE_SEARCH="buildslave"
+elif [ "$DOXY_TYPE" = "stable" ] ; then
+    STACK_TYPE_SEARCH="stable"
+else 
+    STACK_TYPE_SEARCH="beta"
+fi
+echo "STACK_TYPE_SEARCH: $STACK_TYPE_SEARCH"
+echo ""
+
+DATAREL_VERSION=`eups list datarel | grep "$STACK_TYPE_SEARCH" | awk '{print $1}'`
+if [ "X$DATAREL_VERSION" = "X" ]; then
+    echo "Failed to find datarel's $DOXY_TYPE version."
+    exit 1
+fi
+echo "DATAREL_VERSION: $DATAREL_VERSION"
+
+setup datarel $DATAREL_VERSION
+echo ""
+eups list -s
+echo ""
+
+
+$WORK_DIR/$SCM_LOCAL_DIR/bin/makeDocs datarel $DATAREL_VERSION | doxygen -
+if [ ! $? ] ; then
+    echo "Failed to generate DATAREL documentation for $DOXY_TYPE source."
+    exit 1
+fi
+
+# Documentation built, now move it into place
+cd $WORK_DIR/$SCM_LOCAL_DIR/doc
+
+# rename the html directory 
+echo "Move the documentation into web position"
 DOC_DIR="xlink_${DOXY_TYPE}_$DATE" 
 echo "DOC_DIR: $DOC_DIR"
-mv htmlDir  $DOC_DIR
+mv html  $DOC_DIR
 chmod o+rx $DOC_DIR
 
-# send doxygen output directory (formerly: htmlDir) to LSST doc website
-ssh $REMOTE_HOST "mkdir -p $REMOTE_DIR/$DOC_DIR"
-scp -qr $DOC_DIR "$DESTINATION/"
+# send doxygen output directory (formerly: html) to LSST doc website
+ssh $REMOTE_USER@$REMOTE_HOST mkdir -p $REMOTE_DIR/$DOC_DIR
+scp -qr $DOC_DIR "$REMOTE_USER@$DESTINATION/"
 if [ $? != 0 ]; then
     echo "Failed to copy doxygen documentation: $DOC_DIR to $DESTINATION"
     exit 1
 fi
-echo "Doxygen documentation from $DOC_DIR copied to $DESTINATION/$DOC_DIR"
-ssh $REMOTE_HOST "chmod +r $REMOTE_DIR/$DOC_DIR"
+echo "Doxygen documentation from $DOC_DIR copied to $REMOTE_USER@$DESTINATION/$DOC_DIR"
+ssh $REMOTE_USER@$REMOTE_HOST chmod +r $REMOTE_DIR/$DOC_DIR
 
-# Save the actual name of the old doxygen directory
-OLD_DOXY_DOC_DIR=`ssh $REMOTE_HOST "if [ -s $REMOTE_DIR/$SYM_LINK ]; then ls -l $REMOTE_DIR/$SYM_LINK | sed -e 's/^.*-> //' -e 's/ //g'; fi"`
-echo "Old DoxyDir: $OLD_DOXY_DOC_DIR"
+
+# If old sym link exists, save name of actual directory then remove link
+ssh $REMOTE_USER@$REMOTE_HOST  test -e $REMOTE_DIR/$SYM_LINK
+if [ $? == 0 ]; then
+    echo "Old sym link exists, remove it and prepare to remove the actual dir."
+    RET_VALUE=`ssh $REMOTE_USER@$REMOTE_HOST ls -l $REMOTE_DIR/$SYM_LINK | sed -e 's/^.*-> //' -e 's/ //g'`
+    OLD_DOXY_DOC_DIR=`basename $RET_VALUE`
+    ssh $REMOTE_USER@$REMOTE_HOST rm -f $REMOTE_DIR/$SYM_LINK
+fi
 
 # symlink the default xlinkdoxy name to new directory.
-ssh $REMOTE_HOST "rm -f $REMOTE_DIR/$SYM_LINK; ln -s $REMOTE_DIR/$DOC_DIR $REMOTE_DIR/$SYM_LINK"
+echo "Next: ssh $REMOTE_USER@$REMOTE_HOST \"cd $REMOTE_DIR; ln -s  $REMOTE_DIR/$DOC_DIR $SYM_LINK\""
+ssh $REMOTE_USER@$REMOTE_HOST "cd $REMOTE_DIR;ln -s $REMOTE_DIR/$DOC_DIR $SYM_LINK"
 if [ $? != 0 ]; then
     echo "Failed to symlink: $SYM_LINK, to new doxygen documentation: $DOC_DIR"
     exit 1
 fi
 echo "Updated symlink: $SYM_LINK, to point to new doxygen documentation: $DOC_DIR."
 
-# OK, time to remove the old document directory
-#ssh $REMOTE_HOST "if [ -d $OLD_DOXY_DOC_DIR ] ; then rm -r $OLD_DOXY_DOC_DIR; fi"
-ssh $REMOTE_HOST "rm -fr $OLD_DOXY_DOC_DIR"
-if [ $? != 0 ]; then
-    echo "Failed to remove the previous doxy directory: $OLD_DOXY_DOC_DIR"
-    exit 1
-fi
-
+echo ""
+echo "NOTE: crontab should peridocally run a job to remove aged documents."
