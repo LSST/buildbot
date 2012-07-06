de30213c586817b3f64487d50dd5b5960cac0aa6
Robyn Allsman <robyn@LSST.org>
2012-07-05 22:36:15 -0500

    Remove check of satisfactory stack build prior to generating the doxygen documents.  The Check is handled externally to this step. Name will be changed next pass through.

--- scripts/conditionalDoxygen.sh
+++ scripts/conditionalDoxygen.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# this script checks to see all the dependencies for a given package are
-# marked with a BUILD_OK, and if they are, will execute a doxygen build.
-# This is meant to be run from the builds/work directory.
+# this script executes a doxygen build.
+# This is meant to be initiated by the remote buildmaster within a buildslave's
+# 'builds/work' directory. Local environment variables are also passed from the
+# buildmaster.
+
 
 # arguments
 # --package : package we're looking at to see if dependencies are built
@@ -41,46 +43,6 @@ if [ "$PACKAGE" == "" ] || [ "$SCRIPT_DIR" == "" ] || [ "$DOXYGEN_DEST" == "" ] 
     exit 1
 fi
 
-##
-# grab the version of $PACKAGE from the manifest.list file
-##
-VERSION=`grep -w $PACKAGE manifest.list | awk '{ print $2 }'`
-
-if [ "$DEBUG" != "" ]; then
-    echo ls git/$PACKAGE/$VERSION
-fi
-
-##
-# specify the internal dependencies file we're going to use to look up
-# the packages that should all be built properly.
-##
-INTERNAL=git/$PACKAGE/$VERSION/internal.deps
-
-if [ ! -f "$INTERNAL" ]; then
-    echo "error: Can't find $INTERNAL. Exiting."
-    exit 1
-fi
-
-DO_NOT_CONTINUE=0
-while read LINE; do
-    set $LINE
-    if [ "$DEBUG" != "" ]; then
-        echo setup $2 $3
-        echo "Declare $2 $3"
-        echo "checking git/$2/$3/BUILD_OK"
-    fi
-    if [ ! -f "git/$2/$3/BUILD_OK" ]; then
-        echo "error: Package $2 $3 not built."
-        DO_NOT_CONTINUE=1
-    fi
-done < $INTERNAL
-
-if [ "$DO_NOT_CONTINUE" != "0" ]; then
-    echo "Can not continue.  Exiting."
-    exit 1
-fi
-
-# uncomment these when we're ready to run them.
 echo time $SCRIPT_DIR/create_xlinkdocs.sh master $DOXYGEN_DEST $DOXYGEN_URL
 time $SCRIPT_DIR/create_xlinkdocs.sh master $DOXYGEN_DEST $DOXYGEN_URL
 
