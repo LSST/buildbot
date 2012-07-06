053b1028b8e3c718d8fa9100810645d8c19f9bae
Robyn Allsman <robyn@LSST.org>
2012-07-05 22:18:12 -0500

    Change from  only working on the master branch to using any git-branch.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 SCRIPT_DIR=/lsst/home/buildbot/RHEL6/scripts
 
-$SCRIPT_DIR/bootstrap.py >manifest.list
+# -------------------
+# -- get arguments --
+# -------------------
+
+options=$(getopt -l verbose,branch: -- "$@")
+
+while true
+do
+    case $1 in
+        --verbose) VERBOSE=true; shift;;
+        --branch) BRANCH=$2; shift 2;;
+        *) echo "parsed options; arguments left are: $*"
+             break;;
+    esac
+done
+
+echo "Branch: $BRANCH"
+
+$SCRIPT_DIR/bootstrap.py $BRANCH >manifest.list
 sort manifest.list | awk '{print $1}' >manifest.sorted
 echo "==============================================================="
 echo "manifest.list"
 echo "==============================================================="
 cat manifest.sorted
 
-$SCRIPT_DIR/released.py >released.list
+$SCRIPT_DIR/released.py $BRANCH >released.list
 cat released.list | awk '{print $1}' | sort >released.sorted
 echo "==============================================================="
 echo "released.list"
